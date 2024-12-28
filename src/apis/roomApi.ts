fb12bc7fa28c7ae309a1fe504937946ea99a3faa
not-thanhtai <tai606760@gmail.com>
2024-12-28 19:40:59 +0700

    feat: Enhance room and room type APIs with new query parameters and response structures

--- src/apis/roomApi.ts
+++ src/apis/roomApi.ts
@@ -10,12 +10,40 @@ export const roomApi = createApi({
   }),
   endpoints: (builder) => ({
     getRooms: builder.query<IRoomApiResponse, IRoomApiRequest>({
-      query: () => ({
+      query: ({ page, limit, sort }) => ({
         url: '/',
         method: 'GET',
+        params: { page, limit, sort },
       }),
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      merge: (currentCache, newItems) => {
+        currentCache.docs.push(...newItems.docs);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+    }),
+    getRoomsByRoomTypeId: builder.query<IRoomApiResponse, IRoomApiRequest>({
+      query: ({ roomTypeId, page = 1, limit = 10, sort }) => ({
+        url: `/roomType/${roomTypeId}`,
+        method: 'GET',
+        params: { page, limit, sort },
+      }),
+      serializeQueryArgs: ({ endpointName, queryArgs }) => {
+        const { roomTypeId, ...rest } = queryArgs;
+        return { endpointName, roomTypeId, ...rest };
+      },
+      merge: (currentCache, newItems) => {
+        currentCache.docs.push(...newItems.docs);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
   }),
 });
 
-export const { useGetRoomsQuery } = roomApi;
+export const { useGetRoomsQuery, useGetRoomsByRoomTypeIdQuery } = roomApi;
+export const resetRoomsState = roomApi.util.resetApiState;
