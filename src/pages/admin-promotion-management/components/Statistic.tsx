96336f631ca24f2c13f6299d6008483557f2cf4e
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2024-12-30 11:54:02 +0700

    feat: Enhance promotion management with loading states and skeleton components

--- src/pages/admin-promotion-management/components/Statistic.tsx
+++ src/pages/admin-promotion-management/components/Statistic.tsx
@@ -1,14 +1,37 @@
 import { Box } from "@mui/material"
 import StatisticBox from "./StatisticBox"
-
-const Statistic = () => {
+import { IPromotionApiResponse } from "../../../types"
+import React from "react";
+import StatisticBoxSkeleton from "./StatisticBoxSkeleton";
+interface StatisticProps {
+    promotionData: IPromotionApiResponse | undefined;
+}
+const Statistic = ({ promotionData }: StatisticProps) => {
+    const [statistic, setStatistic] = React.useState({ active: 0, inactive: 0, expired: 0 });
+    React.useEffect(() => {
+        if (promotionData) {
+            const now = new Date();
+            const active = promotionData.docs.filter(promotion => new Date(promotion.startDate) < now && new Date(promotion.endDate) > now).length
+            const inactive = promotionData.docs.filter(promotion => new Date(promotion.startDate) > now).length
+            const expired = promotionData.docs.filter(promotion => new Date(promotion.endDate) < now).length
+            setStatistic({ active, inactive, expired })
+        }
+    }, [promotionData])
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 2 }}>
-                <StatisticBox title='Active' value={1000} />
-                <StatisticBox title='Inactive' value={1000} />
-                <StatisticBox title='Expired' value={100} />
-            </Box>
+            {promotionData ? (
+                <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 2 }}>
+                    <StatisticBox title='Active' value={statistic?.active} />
+                    <StatisticBox title='Inactive' value={statistic?.inactive} />
+                    <StatisticBox title='Expired' value={statistic?.expired} />
+                </Box>
+            ) : (
+                <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 2 }}>
+                    <StatisticBoxSkeleton />
+                    <StatisticBoxSkeleton />
+                    <StatisticBoxSkeleton />
+                </Box>
+            )}
         </Box>
     )
 }
