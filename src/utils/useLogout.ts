c01ffc9d31b0602d26a6d9ad89b4d822ac38dd58
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2025-01-01 21:15:36 +0700

    Update useLogout.ts

--- src/utils/useLogout.ts
+++ src/utils/useLogout.ts
@@ -2,14 +2,14 @@ import Cookies from 'js-cookie';
 import { useAppDispatch } from '../stores/store';
 import { logoutSuccess } from '../stores/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { clearTokens } from './tokenUtils';
 
 const useLogout = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        Cookies.remove('accessToken');
-        Cookies.remove('refreshToken');
+        clearTokens();
         Cookies.remove('user');
 
         dispatch(logoutSuccess());
