d9a088968b8744107ddcfac81e64246a28bffa3d
Nguyen Quoc Thang <nguyenqthangwork@gmail.com>
2025-01-03 12:54:41 +0700

    auth reset password

--- src/pages/reset-password/index.tsx
+++ src/pages/reset-password/index.tsx
@@ -1,10 +1,49 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, CircularProgress, Typography } from '@mui/material'
 import TextFieldCustom from '../../components/TextFieldCustom';
 import React from 'react'
+import Cookies from 'js-cookie';
+import { useResetPasswordMutation } from '../../apis/authApi';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const ResetPassword = () => {
+    const token = Cookies.get('reset_password_token');
     const [password, setPassword] = React.useState('');
     const [confirmPassword, setConfirmPassword] = React.useState('');
+    const navigate = useNavigate();
+
+    const validatePassword = () => {
+        if (password === '') {
+            toast.error('Password must not be empty');
+            return false;
+        }
+
+        if (password !== confirmPassword) {
+            toast.error('Password and confirm password do not match');
+            return false;
+        }
+
+        return true;
+    }
+
+    const [resetPassword, { isLoading }] = useResetPasswordMutation();
+
+    const handleResetPassword = async () => {
+        if (!validatePassword()) {
+            return;
+        }
+
+        try {
+            await resetPassword({ token, newPassword: password }).unwrap();
+            toast.success('Password reset successfully');
+            setTimeout(() => {
+                navigate('/login');
+            }, 1000);
+        } catch (error) {
+            toast.error('Failed to reset password');
+        }
+    }
+
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', bgcolor: 'white.50' }}>
             <Box sx={{ width: { xs: '90vw', sm: '50vw' }, bgcolor: 'white.50', padding: '60px', borderRadius: '12px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', boxShadow: '0px 0px 24px rgba(0, 0, 0, 0.1)' }}>
@@ -61,10 +100,13 @@ const ResetPassword = () => {
                         fontWeight: 600,
                         color: 'white.50',
                         bgcolor: 'primary.500',
-                        marginTop: 4
+                        marginTop: 4,
+                        ":disabled": { color: 'gray.200', bgcolor: 'gray.100' }
                     }}
+                    disabled={isLoading}
+                    onClick={handleResetPassword}
                 >
-                    Confirm
+                    {isLoading ? <CircularProgress size={24} color="inherit" /> : 'Confirm'}
                 </Button>
             </Box>
         </Box>
